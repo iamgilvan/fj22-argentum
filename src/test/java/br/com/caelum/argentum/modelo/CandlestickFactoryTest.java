926a712c666029dbb165d9ddfd26506a540b9d44
Gilvan Almeida <g6almeida@hotmail.com>
2017-02-01 00:49:36 -0200

    Testando a Candle com valores iguais.

--- src/test/java/br/com/caelum/argentum/modelo/CandlestickFactoryTest.java
+++ src/test/java/br/com/caelum/argentum/modelo/CandlestickFactoryTest.java
@@ -46,5 +46,23 @@ public class CandlestickFactoryTest {
 
         Assert.assertEquals(0.0, candle.getVolume(), 0.00001);
     }
+/*
+    @Test
+    public void apenasUmaNegociacaoGeraCandleComValoresIguais() {
+	    Calendar hoje = Calendar.getInstance();
 
+	    Negociacao  negociacao1 = new Negociacao(40.5, 100, hoje);
+
+        List<Negociacao> negociacoes = Arrays.asList(negociacao1);
+
+        CandlestickFactory factory = new CandlestickFactory();
+        Candlestick candle = factory.constroiCandleParaData(hoje, negociacoes);
+
+        Assert.assertEquals(40.5, candle.getAbertura(), 0.00001);
+        Assert.assertEquals(40.5, candle.getFechamento(), 0.00001);
+        Assert.assertEquals(40.5, candle.getMinimo(), 0.00001);
+        Assert.assertEquals(45.0, candle.getMaximo(), 0.00001);
+        Assert.assertEquals(4050.0, candle.getVolume(), 0.00001);
+    }
+*/
 }
