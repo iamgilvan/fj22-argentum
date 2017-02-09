c2e51e81546b8e98c04e49bf9ab72fbef57ca757
Gilvan Almeida <g6almeida@hotmail.com>
2017-02-09 15:16:25 -0200

    Lista de negociações de várias datas diferentes e ordenadas por data, quebrar em uma lista de candles, uma para cada data.

--- src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
+++ src/main/java/br/com/caelum/argentum/modelo/CandlestickFactory.java
@@ -1,5 +1,6 @@
 package main.java.br.com.caelum.argentum.modelo;
 
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
@@ -25,5 +26,31 @@ public class CandlestickFactory {
         double fechamento = negociacoes.isEmpty() ? 0 : negociacoes.get(negociacoes.size() -1).getPreco();
 
         return new Candlestick(abertura,fechamento, minimo, maximo, volume, data);
+    }
+
+
+    public List<Candlestick> constroiCandles(List<Negociacao> todasNegociacoes) {
+        List<Candlestick> candles = new ArrayList<Candlestick>();
+
+        List<Negociacao> negociacoesDoDia = new ArrayList<Negociacao>();
+        Calendar dataAtual = todasNegociacoes.get(0).getData();
+
+        for (Negociacao negociacao : todasNegociacoes) {
+            // se não for mesmo dia, fechar candle e reinicia variáveis
+            if (!negociacao.isMesmoDia(dataAtual)){
+                Candlestick candleDoDia = constroiCandleParaData(dataAtual, negociacoesDoDia);
+                candles.add(candleDoDia);
+                negociacoesDoDia = new ArrayList<Negociacao>();
+                dataAtual = negociacao.getData();
+            }
+
+            negociacoesDoDia.add(negociacao);
+        }
+
+        //adiciona último candle
+
+        Candlestick candleDoDia = constroiCandleParaData(dataAtual, negociacoesDoDia);
+        candles.add(candleDoDia);
+        return candles;
     }
 }
