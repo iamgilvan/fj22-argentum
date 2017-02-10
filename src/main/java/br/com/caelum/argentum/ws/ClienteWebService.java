e882a3ac158aaa5901ac9581f8c6ccf0413cd018
Gilvan Almeida <g6almeida@hotmail.com>
2017-02-10 16:10:40 -0200

    Consumindo recursos Web Service das negociações

---
+++ src/main/java/br/com/caelum/argentum/ws/ClienteWebService.java
@@ -0,0 +1,36 @@
+package main.java.br.com.caelum.argentum.ws;
+
+import main.java.br.com.caelum.argentum.modelo.Negociacao;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+
+public class ClienteWebService {
+    //Constante onde é feita a requisição
+    private static final String URL_WEBSERVICE = "http://argentumws.caelum.com.br/negociacoes";
+
+    /*
+    //Retornar Lista de negociações
+    public List<Negociacao> getNegociacoes() throws MalformedURLException {
+        HttpURLConnection connection = null;
+
+        try {
+            URL url = new URL(URL_WEBSERVICE);
+
+            connection = (HttpURLConnection)url.openConnection();
+            InputStream content = connection.getInputStream();
+
+            return new LeitorXML.carrega(content);
+        } catch (IOException e){
+            throw new RuntimeException(e);
+        } finally {
+            connection.disconnect();
+        }
+
+    }
+    */
+}
